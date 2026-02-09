1a13adf389d4d57808f69d11f2bd43fa64d41cb8
OilSteak <159370698+OilSteak@users.noreply.github.com>
2026-02-09 01:58:30 +0000

    Finish task.

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,32 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-enum YesNoResponse: String {
-  case Yes = "y"
-  case No = "n"
-}
-
-/// Gets an input from the user of either yes or no.
-///
-/// Accepts "y" or "n" (and their capitalized versions)
-/// as valid inputs.
-/// 
-///  - Return: Whether they said Yes or No.
-func getYesNoInput(question: String) -> YesNoResponse { 
-  let validResponses: [String] = ["y", "n"]
-
-  while true {
-    // Ask the question.
-    print(question, "\nYes(y, yes) or No(n, no)?")
-
-    if let response = readLine() {
-      if validResponses.contains(response.lowercased()) { 
-        return YesNoResponse(rawValue: response.lowercased())!
-      }
-    }
-  }
-}
-
 
 @main
 struct SwiftPlayground {
@@ -38,55 +12,39 @@ struct SwiftPlayground {
     // Display the prices.
     RevisionTask.printPrices(lunches: lunches)
 
-    // Determine if they are over budget.
     let totalCost = RevisionTask.calculateTotalCost(prices: lunches)
+
+
+    /// ----------------------------------
+    /// Part 6. Snack loop.
+    /// ----------------------------------
+    let minSnackSpend: Double = 10.0
+    let snackPrice: Double = 2.50
+    var snacksTotal: Double = 0.0
+    print()
+    while snacksTotal < minSnackSpend {
+      snacksTotal += snackPrice
+      print("Snack total: $\(snacksTotal)")
+    }
+
+    // Print final totals.
+    print("\nWeekly lunch total: $\(totalCost)")
+    print("Snack total: $\(snacksTotal)")
+    print("Combined total: $\(totalCost + snacksTotal)")
+    print("Average lunch cost: $\(RevisionTask.averageCost(prices: lunches))")
+
+    // Determine if they are over budget.
     if totalCost <= budget {
       print("You stayed within the budget.")
     } else {
       print("You overspent this week :(")
     }
 
-    /// ----------------------------------
-    /// Part 5. Snack loop.
-    /// ----------------------------------
-    let minSnackSpend: Double = 10.0
-    let snackPrice: Double = 2.50
 
-    var snacksTotal: Double = 0.0
-
-
-    // Loop until `snacksTotal` is more than $10
-    var stopBuyingSnacks: Bool = false
-    while !stopBuyingSnacks {
-      // Ask if they want another snack.
-      let buyAnotherSnack: YesNoResponse = getYesNoInput(question: "Buy another snack?")
-      
-      // Check result.
-      if buyAnotherSnack == YesNoResponse.Yes {
-        snacksTotal += snackPrice
-        
-        // Print the new total.
-        print("Total spent on snacks: $\(snacksTotal)")
-
-        // Check if the loop should end.
-        if snacksTotal >= minSnackSpend {
-          // Ask the user if they want to stop buying.
-          let response = getYesNoInput(question: "Stop buying more snacks?") 
-          if response == YesNoResponse.Yes {
-            stopBuyingSnacks = true
-          }
-        }
-      }
+    // Extension: Find most expensive day.
+    if let (day, lunchCost) = lunches.enumerated().max { $0.1 < $1.1 } {
+      print("Most expensive day:")
+      print("Day \(day+1). Lunch chost: $\(lunchCost)")
     }
-
-    print("You have bought \(Int(snacksTotal/snackPrice)) snacks at a cost of \(snacksTotal)")
   }
 }
-
-/*
-
-let numItems = items.count
-
-return totalCost / Double(numItems)
-
-*/
